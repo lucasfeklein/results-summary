4d4eec87c3e5a51fafc4f2b6def95839828649c4
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-12 09:07:59 +0700

    content

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 function App() {
   return (
-    <>
-      <h1 className="">oi</h1>
-    </>
+    <div className="h-screen flex justify-center items-center">
+      <div className="border-red-600 border flex">
+        <div className="flex-1">
+          <h1>Your Result</h1>
+          <p>76</p>
+          <p>of 100</p>
+          <h2>Great</h2>
+          <p>
+            You scored higher than 65% of the people who have taken these test
+          </p>
+        </div>
+        <div className="flex-1">
+          <h2>Summary</h2>
+        </div>
+      </div>
+    </div>
   );
 }
 
