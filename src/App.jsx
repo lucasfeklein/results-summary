00a5d0d1e783d4997778aa19afc71a3bc08c7830
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-12 09:20:01 +0700

    gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 function App() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="border-red-600 border flex">
-        <div className="flex-1">
+      <div className="flex">
+        <div className="flex-1 text-center flex flex-col bg-gradient-to-t from-light-royal-blue to-light-slate-blue">
           <h1>Your Result</h1>
-          <p>76</p>
-          <p>of 100</p>
-          <h2>Great</h2>
+          <div>
+            <p className="text-white">76</p>
+            <p>of 100</p>
+          </div>
+          <h2 className="text-white">Great</h2>
           <p>
             You scored higher than 65% of the people who have taken these test
           </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-slate-blue": "hsl(252, 100%, 67%)",
+        "light-royal-blue": "hsl(241, 81%, 54%)",
+      },
+    },
   },
   plugins: [],
 };
