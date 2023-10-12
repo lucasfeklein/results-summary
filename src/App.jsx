6ce77b0a8e3b85b0a164b79b6aa95fbc5692a455
lucasfeklein <lucas.fklein@hotmail.com>
2023-10-12 13:28:13 +0700

    a

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <div className="flex flex-col h-screen md:h-auto md:flex-row w-screen md:max-w-[610px] shadow-lg rounded-3xl">
+      <div className="flex flex-col h-screen md:h-auto md:flex-row w-screen md:max-w-[610px] shadow-lg md:rounded-3xl">
         <div className="md:flex-1 text-center flex flex-col md:justify-between items-center gap-4 md:gap-6 bg-gradient-to-t from-light-royal-blue to-light-slate-blue p-9 rounded-b-3xl md:rounded-3xl">
           <h1 className="text-secondary text-lg">Your Result</h1>
           <div className="border border-none rounded-full h-[150px] w-[150px] flex flex-col justify-center bg-gradient-to-t from-persian-blue to-violet-blue">
